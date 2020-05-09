5d578af2e30278a8d0a373cc280ce8e3688b1a05
Hsin <ydnicnai@gmail.com>
2020-05-09 16:01:06 +0800

    password_retry ver4

--- password.py
+++ password.py
@@ -1,15 +1,16 @@
 n = 3
 password = 'a123'
 while n > 0 : 
+	n = n-1
 	p = input('請輸入密碼')
 	p = str(p)
 	if p == password :
 		print('登入成功')
 		break
 	else :
-		n = n-1
-		print('密碼錯誤!還有', n ,'次機會')
-		if n == 0 :
+		if n > 0 :
+			print('密碼錯誤!還有', n ,'次機會')
+		elif n == 0 :
 			print('帳號已鎖定')
 
 	
